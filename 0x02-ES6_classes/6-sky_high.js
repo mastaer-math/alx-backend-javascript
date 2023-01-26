908ab5606fdcae16e62ef92ff9a841cf25c74419
Mustapha Dhikrullah <mustapha_dhikrullah@hotmail.com>
2023-01-26 08:12:33 -0800

    ES6 Classes

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,6 +1,6 @@
 import Building from './5-building';
 
-[2;2R[>77;30601;0c]10;rgb:bfbf/bfbf/bfbf]11;rgb:0000/0000/0000export default class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this.floors = floors;
